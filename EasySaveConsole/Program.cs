e199c8dbf170e9bf2b61588317f892ade8885896
CedricNavet <cedric.navet@viacesi.fr>
2019-11-18 10:10:24 +0100

    Console App

---
+++ EasySaveConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Projet_EasySave.Model.Model model = new Model.Model();
+            Projet_EasySave.View.View view = new View.View(model);
+            Projet_EasySave.Controller.Controller controller = new Controller.Controller(model, view);
+        }
+    }
+}
---
+++ EasySaveConsole/View/View.cs
@@ -0,0 +1,39 @@
+﻿using Projet_EasySave.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_EasySave.View
+{
+    public class View
+    {
+        private Model.Model model;
+
+        public View(Model.Model model)
+        {
+            this.model = model;
+        }
+
+        public void Menu()
+        {
+            //List<Model.Travail> temp = .Request("Select * from travail");
+            Travail travail = new Travail();
+            travail.Id = 2;
+            travail.Name = "t";
+            travail.SaveType = "complete";
+            travail.TimeStamping = new DateTime(2019,02,22,00,00,00);
+            travail.TaskName = "n";
+            travail.SourceFile = "s";
+            travail.DestinationFile = "d";
+            travail.FileSize = 1;
+            travail.TransferTime = 1;
+            model.AddTravail(travail);
+            List<Travail> temp = (List<Travail>)model.GetAllTravail();
+            Console.WriteLine(temp.Count);
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+    }
+}
