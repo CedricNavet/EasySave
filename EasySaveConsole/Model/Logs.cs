2ece9de07136be4cf82289ead65506c1c77f2841
AlyssaB <alyssa.binard@viacesi.fr>
2019-11-19 11:10:08 +0100

    Logs

---
+++ EasySaveConsole/Model/Logs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveConsole.Model
+{
+    public class Logs
+    {
+        public DateTime timestamp { get; set; } //horodatage
+        public String nameTable { get; set; } //nom de la table
+        public String sourceFileAddress { get; set; } //adresse fichier source
+        public String destinationFileAddress { get; set; } //adress fichier de destination
+        public byte fileSize { get; set; } //taille fichier
+        public int transferTime { get; set; } //temps de transfert
+    }
+}
