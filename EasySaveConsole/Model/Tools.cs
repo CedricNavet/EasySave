424928c8b287e7a7b40f2993bb6b98425cf22d1f
CedricNavet <cedric.navet@viacesi.fr>
2019-11-19 12:25:38 +0100

    Tools

--- EasySaveConsole/Model/Tools.cs
+++ EasySaveConsole/Model/Tools.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EasySaveConsole.Controller
+namespace EasySaveConsole.Model
 {
     public static class Tools
     {
