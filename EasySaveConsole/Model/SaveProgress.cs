6551c59f486961c848ace270d28d64e24b02eed5
AlyssaB <alyssa.binard@viacesi.fr>
2019-11-19 11:23:48 +0100

    saveprogress

--- EasySaveConsole/Model/SaveProgress.cs
+++ EasySaveConsole/Model/SaveProgress.cs
@@ -8,6 +8,11 @@ namespace EasySaveConsole.Model
 {
     public class SaveProgress
     {
-
+        public DateTime timestamp { get; set; } //horodatage
+        public int numberTotalFiles { get; set; } //Nb fichier totaux
+        public byte totalFileSize { get; set; } //Taille fichier totaux
+        public int numberRemainFiles { get; set; } //Nb fichier restant
+        public byte remainFileSize { get; set; } //Taille fichier restant
+        public String currentFileName { get; set; } //Nom du fichier en cours
     }
 }
